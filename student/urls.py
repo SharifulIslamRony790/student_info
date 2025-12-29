9b524925e5c247609a51ebf6005ce65cf321ea8d
Md Shariful Islam Rony <mdsharifulislamrony790@gmail.com>
2025-12-30 01:12:45 +0600

    Add Excel export feature

--- student/urls.py
+++ student/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
+from .views import StudentViewSet, export_students_excel
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('export-excel/', export_students_excel, name='export-student-excel'),
 ]
 
--- student/views.py
+++ student/views.py
@@ -5,3 +5,24 @@ from .serializers import StudentSerializer
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+import openpyxl
+from django.http import HttpResponse
+
+def export_students_excel(request):
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="students.xlsx"'
+
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = "Students"
+
+    # Header
+    ws.append(['Name', 'Roll', 'City'])
+
+    students = Student.objects.all()
+    for student in students:
+        ws.append([student.name, student.roll, student.city])
+
+    wb.save(response)
+    return response
